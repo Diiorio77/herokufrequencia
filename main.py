f43952e7b7140befaf279f24a1b850b35bad40fb
diiorio7 <gabrieliorio1@hotmail.com>
2021-10-10 22:57:08 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import pickle
+
+
+boost =  pickle.load(open("D:/data_science/deploy_frequencia/modelo.sav", "rb"))
+
+colunas = ["maximo", "minimo", "frequencia", "aumento_frequencia"]
+
+app = Flask(__name__)
+
+@app.route('/')
+def funcao():
+    return "API Para Retorno das previsões do modelo de regressão"
+
+
+@app.route('/previsao/', methods=['POST'])
+def bpm():
+    informacao = request.get_json()
+    informacao_input = [informacao[col] for col in colunas]
+    bpm_previsto = boost.predict([informacao_input])
+    return jsonify(previsao=bpm_previsto[0].round(2))
+
+
+
+app.run(debug=True)
+
+
